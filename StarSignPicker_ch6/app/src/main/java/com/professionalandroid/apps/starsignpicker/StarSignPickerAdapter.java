effd923584d3f6f4fc56ad84089cf7ee90a08ff8
shenbingyan <shenby@server03>
2020-05-14 17:59:07 +0800

    StarSignPicker_ch6  code format

--- StarSignPicker_ch6/app/src/main/java/com/professionalandroid/apps/starsignpicker/StarSignPickerAdapter.java
+++ StarSignPicker_ch6/app/src/main/java/com/professionalandroid/apps/starsignpicker/StarSignPickerAdapter.java
@@ -26,68 +26,68 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class StarSignPickerAdapter extends RecyclerView.Adapter<StarSignPickerAdapter.ViewHolder> {
-  private String[] mStarSigns = {"Aries", "Taurus", "Gemini", "Cancer",
-    "Leo", "Virgo", "Libra", "Scorpio",
-    "Sagittarius", "Capricorn", "Aquarius",
-    "Pisces"};
+    IAdapterItemClick mAdapterItemClickListener;
+    private String[] mStarSigns = {"Aries", "Taurus", "Gemini", "Cancer",
+            "Leo", "Virgo", "Libra", "Scorpio",
+            "Sagittarius", "Capricorn", "Aquarius",
+            "Pisces"};
 
-  public StarSignPickerAdapter() {
-  }
-
-  @Override
-  public int getItemCount() {
-    return mStarSigns == null ? 0 : mStarSigns.length;
-  }
-
-  @NonNull
-  @Override
-  public StarSignPickerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    // Create the new View
-    View v = LayoutInflater.from(parent.getContext())
-               .inflate(R.layout.list_item_layout, parent, false);
-
-    return new ViewHolder(v, null);
-  }
-
-  @Override
-  public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
-    holder.textView.setText(mStarSigns[position]);
-
-    holder.mListener = new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (mAdapterItemClickListener != null)
-          mAdapterItemClickListener.onItemClicked(mStarSigns[position]);
-      }
-    };
-  }
-
-  public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    public TextView textView;
-    public View.OnClickListener mListener;
-
-    public ViewHolder(View v, View.OnClickListener listener) {
-      super(v);
-      mListener = listener;
-      textView = v.findViewById(R.id.itemTextView);
-      v.setOnClickListener(this);
+    public StarSignPickerAdapter() {
     }
 
     @Override
-    public void onClick(View v) {
-      if (mListener != null)
-        mListener.onClick(v);
+    public int getItemCount() {
+        return mStarSigns == null ? 0 : mStarSigns.length;
     }
-  }
 
-  public interface IAdapterItemClick {
-    void onItemClicked(String selectedItem);
-  }
+    @NonNull
+    @Override
+    public StarSignPickerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Create the new View
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_layout, parent, false);
 
-  IAdapterItemClick mAdapterItemClickListener;
+        return new ViewHolder(v, null);
+    }
 
-  public void setOnAdapterItemClick(
-    IAdapterItemClick adapterItemClickHandler) {
-    mAdapterItemClickListener = adapterItemClickHandler;
-  }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(mStarSigns[position]);
+        final int position2 = position;
+
+        holder.mListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAdapterItemClickListener != null)
+                    mAdapterItemClickListener.onItemClicked(mStarSigns[position2]);
+            }
+        };
+    }
+
+    public void setOnAdapterItemClick(
+            IAdapterItemClick adapterItemClickHandler) {
+        mAdapterItemClickListener = adapterItemClickHandler;
+    }
+
+    public interface IAdapterItemClick {
+        void onItemClicked(String selectedItem);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView textView;
+        public View.OnClickListener mListener;
+
+        public ViewHolder(View v, View.OnClickListener listener) {
+            super(v);
+            mListener = listener;
+            textView = v.findViewById(R.id.itemTextView);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mListener != null)
+                mListener.onClick(v);
+        }
+    }
 }
